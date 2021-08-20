0d2a0a16e4aab572aea948bbaa955571f299ca15
rileyale001 <alexjtriley@gmail.com>
2021-08-20 00:39:51 -0700

    committing on main

---
+++ Develop/models/index.js
@@ -0,0 +1,44 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+
+  foreignKey: 'category_id'
+});
+// Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+ 
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+    unique: false,
+   
+  },
+  as: 'tags'
+});
+
+// Tags belongsToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tags'
+});
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
